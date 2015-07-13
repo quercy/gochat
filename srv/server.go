687aadc16a4a60c929024c13421e1b7626e75b5c
quercy <reid.savage@gmail.com>
2015-07-13 12:55:28 -0400

    server in workable state

--- srv/server.go
+++ srv/server.go
@@ -1,99 +1,116 @@
-// @todo implement logger
-// @todo robusticise users
-// make stateless, to handle multiple connections?3
-
 package gochat
 
 import(
 	"net"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"bufio"
 	"strings"
 )
 
-type user struct {
-	username string
-	userid string
-	online bool
-	connection net.Conn
-}
-
-// Adds a new user to the specified user collection - returns that user
-func  (UC *userCollection)  AddUser (name string, address string, cn net.Conn) {
-	// @todo log: adding a user
-	u := user{username:name, userid:address, online:true, connection:cn}
-	UC._users = append(UC._users, &u)
-}
-
-type userCollection struct {
-	_users []*user
+func init() {
+	// create logger
+	InitLogger(os.Stdout, os.Stdout, os.Stdout, os.Stderr)
+	Trace.Println("Logger initialized")
 }
 
 func RunServer() {
-	// create logger
-	InitLogger(ioutil.Discard, os.Stdout, os.Stdout, os.Stderr)
-	// listen
+	Trace.Println("Listening on port 8080")
 	ln, errors := net.Listen("tcp", ":8080")
-	// create the user collection
-	uc := userCollection{_users:make([]*user, 0)}
-
 	if errors != nil {
-		panic(errors) // ???
+		Error.Println("Errors while listening: ",errors)
+		os.Exit(1)
 	}
+	Trace.Println("Creating userCollection with _users:make([]*user, 0)")
+	uc := userCollection{_users:make([]*user, 0)}
 //	ch := make(chan []byte, 5)
 //	go broadcast(ch, uc)
 	for {
-		connection, error := ln.Accept()
-		if errors != nil {
-			fmt.Println(error)
+		connection, err := ln.Accept()
+		if err != nil {
+			Error.Println("Errors while connecting: ",error)
+			os.Exit(1)
 		} else {
 			// start chatting
+			Info.Println("New connection established")
 			go handleConnection (connection, &uc)
 		}
 	}
 }
 
+
+type userCollection struct {
+	_users []*user
+}
+
+type user struct {
+	username string
+	user_network_location string
+	online bool
+	connection net.Conn
+}
+
+// Adds a new user to the specified user collection - returns that user
+func  (UC *userCollection)  AddUser (name string, address string, cn net.Conn) *user {
+	u := user{username:name, user_network_location:address, online:true, connection:cn}
+	Trace.Println("Adding user ", u, " to userCollection")
+	UC._users = append(UC._users, &u)
+	return &u
+}
+
 func handleConnection (connection net.Conn, uc *userCollection) {
-	messages := 0
-	connection.Write([]byte("Enter your username: "))
-	readUsername := false
-	username := ""
-	for {
-		input, is_command := readInput(connection)
-		if(readUsername == false) { // only in first iteration
-			uc.AddUser(input, connection.RemoteAddr().String(), connection)
-			username = input
-			welcomeMessage := fmt.Sprintf("%v has entered the chat.", username)
-			uc.broadcast(welcomeMessage, "")
-//			ch <- []byte(welcomeMessage)
-			readUsername = true
-		} else if(!is_command) {
-			text := fmt.Sprintf("%v: %s", username, input)
-			uc.broadcast(text, username)
-//			ch <- []byte(text) // send to the broadcast stream
-		} else {
-//			 user has entered a command
-			if(input == "/quit") {
-				connection.Write([]byte("Goodbye.\n"))
-				defer connection.Close()
-				break
-			}
+
+	var usr *user;
+
+	initial_loop: for {
+		connection.Write([]byte("Enter your username: "))
+		input, _ := readInput(connection)
+		usr = uc.AddUser(input, connection.RemoteAddr().String(), connection)
+		welcomeMessage := fmt.Sprintf("%v has entered the chat.", input)
+		uc.broadcast(welcomeMessage, "")
+		//	ch <- []byte(welcomeMessage)
+		break initial_loop
+	}
+
+	connection_loop: for {
+		if !usr.online {
+			break connection_loop
 		}
 
-		messages++
-		if(messages == 5) {
-			connection.Write([]byte("You have exceeded your message quota! Goodbye.\n"))
-			defer connection.Close()
-			break
+		input, is_command := readInput(connection)
+
+		if(!is_command) {
+			text := fmt.Sprintf("%v: %s", usr.username, input)
+			uc.broadcast(text, usr.username)
+//			ch <- []byte(text) // send to the broadcast stream
+		} else {
+			command := strings.Split(input, " ")
+			switch command[0] {
+				case "/quit":
+					connection.Write([]byte("Goodbye.\n"))
+					uc.broadcast("User " + usr.username + " has left the channel.", usr.username)
+					defer connection.Close()
+					break connection_loop
+				case "/list":
+					connection.Write([]byte(uc.getUserListString()))
+				case "/kick":
+					if len(command) > 1 {
+						result := uc.kickUser(command[1])
+						if result {
+							connection.Write([]byte("You kicked " + command[1] + ".\n"))
+						} else {
+							connection.Write([]byte("No such user is online.\n"))
+						}
+					}
+			}
 		}
 	}
 }
 
+// Broadcasts a message to all connected users : takes the message as a string and the username of the sender
 func (uc *userCollection) broadcast (msg string, usr string) {
 	for i := 0; i < len(uc._users); i++ {
-		if uc._users[i].username != usr {
+		if uc._users[i].username != usr { // when called, no need to broadcast to the sending user (usually)
 			uc._users[i].connection.Write([]byte(msg + "\n")) // getting there
 		}
 	}
@@ -102,5 +119,27 @@ func (uc *userCollection) broadcast (msg string, usr string) {
 func readInput(cn net.Conn) (string, bool) {
 	buf := bufio.NewReader(cn)
 	output, _ := buf.ReadString('\n')
-	return strings.TrimSpace(output), false // hacky way to remove newline
+	output = strings.TrimSpace(output)
+	return output, strings.HasPrefix(output,"/")
+}
+
+func (uc *userCollection) getUserListString() string {
+	output := "Username :: Online :: Network Location"
+	for i := 0; i < len(uc._users); i++ {
+		line := fmt.Sprintf("%v :: %v :: %v ", uc._users[i].username, uc._users[i].online, uc._users[i].user_network_location)
+		output = strings.Join([]string{output,line}, "\n")
+	}
+	return strings.Join([]string{output,""}, "\n")
+}
+
+func (uc *userCollection) kickUser (userString string) bool {
+	for _, u := range uc._users {
+		if u.username == userString && u.online == true {
+			u.connection.Write([]byte("You have been kicked.\n"))
+			u.online = false
+			u.connection.Close()
+			return true;
+		}
+	}
+	return false;
 }
